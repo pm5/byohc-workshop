8039d5397eeaee54145ef1a3a3ad8bb47d0c40ce
Pomin Wu <pomin5@gmail.com>
2015-10-07 11:03:25 +0800

    add tests

--- lambda/lambda_test.go
+++ lambda/lambda_test.go
@@ -87,8 +87,34 @@ func TestBooleanNot(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	n := NewNode(expr)
-	if r := NormalForm(n); fmt.Sprintf("%s", r) != `(\m__ (\m_ m_))` {
+	r := NormalForm(NewNode(expr))
+	if rr := r.(Lambda).Body.(Lambda); rr.Argument != rr.Body.(Var).Name {
 		t.Errorf("Incorrect evaluation `%s`", r)
 	}
+}
+
+func dec(node Node) Node {
+	return NormalForm(NewApp(NewApp(node, NewLambda("x", NewVar("x"))), NewVar("_")))
+}
+
+func TestNaturalNumber(t *testing.T) {
+	var expr []interface{}
+	err := json.Unmarshal([]byte(`["app",["lam","+1",["app",["lam","0",["app",["lam","1",["app",["lam","2",["app",["lam","3",["app",["lam","4",["app",["lam","5",["app",["lam","6",["app",["lam","7",["app",["lam","8",["app",["lam","9",["app",["lam","-1",["app",["var","-1"],["app",["var","-1"],["app",["var","-1"],["app",["var","+1"],["app",["var","+1"],["app",["var","+1"],["app",["var","-1"],["app",["var","-1"],["app",["var","-1"],["app",["var","-1"],["var","9"]]]]]]]]]]]],["lam","n",["app",["app",["var","n"],["lam","n-",["var","n-"]]],["var","0"]]]]],["app",["var","+1"],["var","8"]]]],["app",["var","+1"],["var","7"]]]],["app",["var","+1"],["var","6"]]]],["app",["var","+1"],["var","5"]]]],["app",["var","+1"],["var","4"]]]],["app",["var","+1"],["var","3"]]]],["app",["var","+1"],["var","2"]]]],["app",["var","+1"],["var","1"]]]],["app",["var","+1"],["var","0"]]]],["lam","s",["lam","z",["var","z"]]]]],["lam","n",["lam","s",["lam","z",["app",["var","s"],["var","n"]]]]]]`), &expr)
+	if err != nil {
+		t.Error(err)
+	}
+	r := NormalForm(NewNode(expr))
+	z := dec(dec(dec(dec(dec(r)))))
+	if z := z.(Lambda).Body.(Lambda); z.Argument != z.Body.(Var).Name {
+		t.Errorf("%s\n", r)
+	}
+}
+
+func TestInfiniteSequencel(t *testing.T) {
+	var expr []interface{}
+	err := json.Unmarshal([]byte(`["app",["lam","+1",["app",["lam","0",["app",["lam","1",["app",["lam","2",["app",["lam","3",["app",["lam","4",["app",["lam","5",["app",["lam","6",["app",["lam","7",["app",["lam","8",["app",["lam","9",["app",["lam","nil",["app",["lam","cons",["app",["lam","Y",["app",["lam","take",["app",["lam","map",["app",["lam","0-1-2-",["app",["app",["var","take"],["var","5"]],["var","0-1-2-"]]],["app",["var","Y"],["lam","0-1-2-",["app",["app",["var","cons"],["var","0"]],["app",["app",["var","map"],["var","+1"]],["var","0-1-2-"]]]]]]],["lam","f",["app",["var","Y"],["lam","go",["lam","ls",["app",["app",["var","ls"],["lam","a",["lam","as",["app",["app",["var","cons"],["app",["var","f"],["var","a"]]],["app",["var","go"],["var","as"]]]]]],["var","nil"]]]]]]]],["app",["var","Y"],["lam","take",["lam","n",["lam","ls",["app",["app",["var","n"],["lam","n-",["app",["app",["var","ls"],["lam","a",["lam","as",["app",["app",["var","cons"],["var","a"]],["app",["app",["var","take"],["var","n-"]],["var","as"]]]]]],["var","nil"]]]],["var","nil"]]]]]]]],["lam","f",["app",["lam","x",["app",["var","f"],["app",["var","x"],["var","x"]]]],["lam","x",["app",["var","f"],["app",["var","x"],["var","x"]]]]]]]],["lam","a",["lam","as",["lam","is-cons",["lam","is-nil",["app",["app",["var","is-cons"],["var","a"]],["var","as"]]]]]]]],["lam","is-cons",["lam","is-nil",["var","is-nil"]]]]],["app",["var","+1"],["var","8"]]]],["app",["var","+1"],["var","7"]]]],["app",["var","+1"],["var","6"]]]],["app",["var","+1"],["var","5"]]]],["app",["var","+1"],["var","4"]]]],["app",["var","+1"],["var","3"]]]],["app",["var","+1"],["var","2"]]]],["app",["var","+1"],["var","1"]]]],["app",["var","+1"],["var","0"]]]],["lam","s",["lam","z",["var","z"]]]]],["lam","n",["lam","s",["lam","z",["app",["var","s"],["var","n"]]]]]]`), &expr)
+	if err != nil {
+		t.Error(err)
+	}
+	_ = NormalForm(NewNode(expr))
 }
